f18e5f940176601828f1eaf6edf425fc9083ccee
ZhiXiang <zhixiang8897@gmail.com>
2022-04-27 03:25:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
     );
@@ -13,8 +16,15 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
